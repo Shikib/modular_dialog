c18f452d16cd92198ff6bb2634d5d50529f1a233
Shikib Mehri <mehrishikib@gmail.com>
2019-03-20 17:26:01 -0400

    minor changes for evaluation

--- evaluate.py
+++ evaluate.py
@@ -5,14 +5,14 @@ import random
 
 from nlp_utils import *
 
-predict_f = open('attn_19_predicted_sentences.txt')
-predicted_sentences = [x.strip() for x in predict_f][:-1]
+predict_f = open('baseline_19_predicted_sentences.txt')
+predicted_sentences = [[x.strip()] for x in predict_f][:-1]
 
 target_f = open('attn_19_target_sentences.txt')
-target_sentences = [x.strip() for x in target_f][:-1]
+target_sentences = [[x.strip()] for x in target_f][:-1]
 
 bscorer = BLEUScorer()
 
-bleu_score = bscorer.score(target_sentences, target_sentences)
+bleu_score = bscorer.score(predicted_sentences, target_sentences)
 
 print("Test BLEU score: {0:.10f}".format(bleu_score))
--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import math
 import model
 import random
 
+from collections import defaultdict
+
 def str2bool(v):
   if v.lower() in ('yes', 'true', 't', 'y', '1'):
     return True
@@ -35,7 +37,7 @@ args = parser.parse_args()
 def load_data(filename):
   data = json.load(open(filename))
   rows = []
-  for dial in data.values():
+  for filename,dial in data.items():
     input_so_far = []
     for i in range(len(dial['sys'])):
       input_so_far += ['_GO'] + dial['usr'][i].strip().split() + ['_EOS']
@@ -45,7 +47,7 @@ def load_data(filename):
       db = dial['db'][i]
       bs = dial['bs'][i]
 
-      rows.append((input_seq, target_seq, db, bs))
+      rows.append((input_seq, target_seq, db, bs, filename, i))
 
       # Add sys output
       input_so_far += target_seq
@@ -69,7 +71,8 @@ policy = model.Policy(hidden_size=args.hid_size,
 
 decoder = model.Decoder(emb_size=args.emb_size,
                         hid_size=args.hid_size,
-                        vocab_size=len(output_w2i))
+                        vocab_size=len(output_w2i),
+                        use_attn=args.use_attn)
 
 model = model.Model(encoder=encoder,
                     policy=policy,
@@ -86,10 +89,10 @@ train = load_data('data/train_dials.json')
 valid = load_data('data/val_dials.json')
 test = load_data('data/test_dials.json')
 
-predict_file = open('{0}_predicted_sentences.txt'.format(args.model_name), 'w')
-target_file = open('{0}_target_sentences.txt'.format(args.model_name), 'w')
 num_batches = math.ceil(len(test)/args.batch_size)
 indices = list(range(len(test)))
+
+all_predicted = defaultdict(list)
 for batch in range(num_batches):
   # Prepare batch
   batch_indices = indices[batch*args.batch_size:(batch+1)*args.batch_size]
@@ -99,12 +102,8 @@ for batch in range(num_batches):
   # Get predicted sentences for batch
   predicted_sentences = model.decode(input_seq, input_lens, target_seq, target_lens, db, bs)
 
-  target_sentences = [' '.join(row[1][1:-1]) for row in batch_rows]
+  # Add predicted to list
+  for i,sent in enumerate(predicted_sentences):
+    all_predicted[batch_rows[i][-2]].append(sent) 
 
-  # Write predicted and target sentences to output file
-  for p, t in zip(predicted_sentences, target_sentences):
-    predict_file.write(p+'\n')
-    target_file.write(t+'\n')
-
-predict_file.close()
-target_file.close()
+json.dump(all_predicted, open('{0}_predictions.json'.format(args.model_name), 'w+'))
